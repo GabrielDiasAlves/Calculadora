48511116280df8c8e9124938bbe9e4a55526aaac
Gabriel <gabrieldalves@outlook.com.br>
2022-02-20 02:12:34 -0300

    Finalização da parte gráfica

--- src/br/com/gabriel/calc/visao/Calculadora.java
+++ src/br/com/gabriel/calc/visao/Calculadora.java
@@ -1,17 +1,34 @@
 package br.com.gabriel.calc.visao;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 @SuppressWarnings("serial")
 public class Calculadora extends JFrame {
 
 	public Calculadora() {
-		setSize(232, 322);						 //Define o tamanho da tela
+		
+		organizarLayout();
+		
+		setSize(232, 322); 						 //Define o tamanho da tela
 		setDefaultCloseOperation(EXIT_ON_CLOSE); //Finaliza o processo/janela automaticamente 
-		setLocationRelativeTo(null);			 //Faz com que inicialize no centro da tela do computador
-		setVisible(true); 						 //Habilita a tela 
+		setLocationRelativeTo(null); 			 //Faz com que inicialize no centro da tela do computador
+		setVisible(true);  					  	 //Habilita a tela 
 	}
 	
+	private void organizarLayout() {
+		setLayout(new BorderLayout());
+
+		Display display = new Display();
+		display.setPreferredSize(new Dimension(233, 60));
+		add(display, BorderLayout.NORTH);
+		
+		Teclado teclado = new Teclado();
+		add(teclado, BorderLayout.CENTER);
+	}
+
 	public static void main(String[] args) {
 		new Calculadora();
 	}
--- src/br/com/gabriel/calc/visao/Display.java
+++ src/br/com/gabriel/calc/visao/Display.java
@@ -1,8 +1,24 @@
 package br.com.gabriel.calc.visao;
 
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Display extends JPanel {
 
+	private final JLabel label;
+	
+	public Display() {
+		label = new JLabel("123");
+		setBackground(new Color(46, 49, 50)); 				 //Definindo cor do fundo
+		label.setForeground(Color.WHITE); 					 //Definindo cor do texto
+		label.setFont(new Font("courier", Font.PLAIN, 30));  //Definindo a fonte
+		setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 20)); //Definindo o local de exibição
+		add(label); 										 //Adicionando na tela
+	}
+	
 }
